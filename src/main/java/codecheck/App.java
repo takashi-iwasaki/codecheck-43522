c45c3146682b3917700e4fedfd7236c1d1415bd6
tie302395 <tie302395@S1612C11975-T1.tisnt.tis.co.jp>
2018-06-19 14:00:49 +0900

    test2

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,11 +3,8 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("Hello %s \!", i, args[i]);
+			String output = String.format("Hello %s !", i, args[i]);
 			System.out.println(output);
-//test
-
-
 		}
 	}
 }
